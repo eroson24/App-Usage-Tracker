49b1a4ef08e32ab4a259df7aeea7fdb8dd243b79
eroson24 <erolsonmeznj@gmail.com>
2025-05-04 11:47:27 -0400

    Commit

--- script.py
+++ script.py
@@ -32,7 +32,7 @@ def main():
         )
         creds = flow.run_local_server(port=0)
 
-    # Save the credentials for the next run
+    # Save the credentials for the next run.
     with open("token.json", "w") as token:
       token.write(creds.to_json())
 
