e4015b772f3b9c0fde85dbc398cd64c934882c49
eroson24 <erolsonmeznj@gmail.com>
2025-05-04 11:43:52 -0400

    Initial Google calendar API integration

--- script.py
+++ script.py
@@ -1,1 +1,46 @@
-print("initial commit")
+import datetime as dt
+import os.path
+import time
+import win32gui as win
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+def get_active_window_title():
+    activeWindow = win.GetForegroundWindow()
+    return win.GetWindowText(activeWindow)
+
+print("Your active window at the moment is " + get_active_window_title())
+
+def main():
+    # Google Calendar authentication
+    creds = None
+
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json")
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+    try:
+        service = build("calendar", )
+    except HttpError as error: 
+        print("An error occurred :( - ", error)
+
+        
+
+
